8b16dc557cfecb1be8c11cb47d5d437a2343f6e2
BSKorolev <90824350+BSKorolev@users.noreply.github.com>
2021-10-25 03:28:57 +0200

    41

---
+++ 41.cpp
@@ -0,0 +1,60 @@
+﻿
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+#define N 1000
+int main()
+{
+    int n;
+    bool flag=true;
+    int mas[N];
+    cin >> n;
+    for (int i=0; i < n; i++)
+    {
+        cin >> mas[i];
+    }
+    
+    for (int i=0; i < n; i++)
+    {
+        int x=0;
+        for (int j=2; j < mas[i]; j++)
+        {
+            if (mas[i] % j == 0)
+                x += 1;
+            
+        }
+        
+        if (x > 1)
+        {
+            continue;
+        }
+        if (x==1);
+        {
+            cout << "est prostoe" << endl;
+            flag=false;
+            break;
+        }
+
+    }
+    int tmp = 0;
+    if (flag==true)
+    {
+        for (int i = 0; i < n - 1; i++)
+            for (int j = i + 1; j < n; j++)
+            {
+                if (mas[i] > mas[j])
+                {
+                    tmp = mas[i];
+                    mas[i] = mas[j];
+                    mas[j] = tmp;
+
+                }
+
+            }
+    }
+    for (int i = 0; i < n; i++)
+        cout << mas[i] << " ";
+}
+
