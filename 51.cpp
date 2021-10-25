ab6259598ed456f112b3d0bb8d79a18f434b83c8
BSKorolev <90824350+BSKorolev@users.noreply.github.com>
2021-10-25 03:29:45 +0200

    51

---
+++ 51.cpp
@@ -0,0 +1,37 @@
+﻿
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <windows.h>
+
+using namespace std;
+int main()
+{
+	char S[100];
+	char c;
+	int x=1;
+	bool flag = false;
+	cin >> S;
+	cin >> c;
+
+	for (int i = 0; i < strlen(S); i++)
+         if (S[i] == c)
+		 {
+			flag = true;
+			x = i+1;
+			break;
+		 }
+
+
+
+	if (flag)
+
+		cout << x;
+
+	else
+
+		cout << "takogo simvola net";
+}
+
+
